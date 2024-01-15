830292d7806edb039007d38af6027e36d21cb07f
sobgui <i_sobgui@hetic.eu>
2024-01-15 13:33:54 +0000

    Adding Global constants server for The Game

---
+++ server/src/config/constants.py
@@ -0,0 +1,11 @@
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb
+WOOD = (153, 101, 21) # wood color
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128,128,128)
