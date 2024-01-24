2ebb0ebc4a905147ef6eb928b0c3995ecf6ac395
VPoussou <v_poussou@hetic.eu>
2024-01-24 14:09:23 +0100

    Updating semi code to full code for Piece model for the game client side

--- client/src/model/piece.py
+++ client/src/model/piece.py
@@ -12,13 +12,18 @@ class Piece:
         self.calc_pos()
 
     def calc_pos(self):
-        pass
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
 
     def make_king(self):
         self.king = True
 
     def draw(self, win):
-        pass
+        radius = SQUARE_SIZE // 2 - self.PADDING
+        pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
+        pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width() // 2, self.y - CROWN.get_height() // 2))
 
     def move(self, row, col):
         self.row, self.col = row, col
