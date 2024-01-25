25df883dd10f5a0409f15244070e2b0497d33ddb
Wenderz <lazare.nogrette@epita.fr>
2024-01-25 10:12:44 +0100

    updated code for piece class server side

--- server/src/model/piece.py
+++ server/src/model/piece.py
@@ -4,5 +4,16 @@ from config.constants import SQUARE_SIZE
 class Piece:
     def __init__(self):
         pass
+    # Calculate the position of the piece on the board
+    def calc_pos(self, args):
+        col = args['col']
+        row = args['row']
+        x = SQUARE_SIZE * col + SQUARE_SIZE // 2
+        y = SQUARE_SIZE * row + SQUARE_SIZE // 2
+        position = {
+            'x': x,
+            'y': y
+        }
+        return position
     
 
