6443f71e39fcf6a10e3d2329a4c6c0240733ae1a
sobgui <i_sobgui@hetic.eu>
2024-01-25 18:10:25 +0000

    Updating semi code for Board model( move) for the game client side

--- client/src/model/board.py
+++ client/src/model/board.py
@@ -12,16 +12,43 @@ class Board:
         self.create_board()
 
     def draw_squares(self, win):
-        pass
+        win.fill(BLACK)
+        for row in range(ROWS):
+            for col in range(row % 2, COLS, 2):
+                pygame.draw.rect(win, WOOD, (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
     def move(self, piece, row, col):
-        pass
+        self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][piece.col]
+        piece.move(row, col)
+
+        if row == ROWS - 1 or row == 0:
+            piece.make_king()
+            if piece.color == WHITE:
+                self.white_kings += 1
+            else:
+                self.red_kings += 1
 
     def get_piece(self, row, col):
         return self.board[row][col]
 
     def create_board(self):
-        pass
+        is_game_config = self.game_api.init_game()
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                if col % 2 == ((row + 1) % 2):
+                    if row < 3:
+                        # piece = Piece(self.game_api, row, col, WHITE)
+                        # print(f'row < 3: {piece}')
+                        self.board[row].append(Piece(self.game_api, row, col, WHITE))
+                    elif row > 4:
+                        # piece = Piece(self.game_api, row, col, WOOD)
+                        # print(f'row > 4: {piece}')
+                        self.board[row].append(Piece(self.game_api, row, col, WOOD))
+                    else:
+                        self.board[row].append(0)
+                else:
+                    self.board[row].append(0)
 
     def draw(self, win):
         self.draw_squares(win)
@@ -34,6 +61,8 @@ class Board:
     def remove(self, pieces):
         pass
 
+        # print(f'Available Pieces to remove: {pieces}')
+
     def winner(self):
         if self.red_left <= 0:
             return WHITE
@@ -43,3 +72,4 @@ class Board:
         return None
 
     # Method to get valid moves for a given piece
+
