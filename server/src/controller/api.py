e94556b159cf4f5e1c9fe9f2c18008f2ae99b039
sobgui <i_sobgui@hetic.eu>
2024-01-30 09:02:49 +0000

    Updating API file controller for server side

--- server/src/controller/api.py
+++ server/src/controller/api.py
@@ -1,29 +1,94 @@
 # controller/api.py
 from flask import jsonify, request
-from config import app
-from model.game import Game
+from src.config import app
+from src.model.game import Game
 
 game = Game()
 
 @app.route('/game/init', methods=['GET'])
 def init_game():
     # Access the "value" parameter from the query string
-        return jsonify({})
+    try:
+        #value = request.args.get('config')
+        # print('Init Game: ', value)
+        print('----------Init Game--------------')
+
+        game = Game()
+        # config = game.config
+        config = {
+            'white_left': game.Board.white_left,
+            'red_left': game.Board.red_left,
+            'nbr_rows': game.Board.rows,
+            'nbr_cols': game.Board.cols,
+            'windows_size': game.Board.window_size,
+            'piece_color': game.Board.piece_color
+        }
+        print('The Game config is: ', config)
+        print('-----------------------')
+        return jsonify({'config': config})
+    except Exception as e:
+        print('Error: ', str(e))
+        print('-----------------------')
+        # return jsonify({'result': 'error', 'message': str(e)})
+        return jsonify({'error': str(e)})
 
 
 @app.route('/game/change_turn', methods=['GET'])
 def get_turn():
     # Access the "value" parameter from the query string
-    return jsonify({})
+    try:
+        value = request.args.get('turn')
+        print('Value of turn: ', value)
+        print('-----------------------')
+        turn = game.change_turn(value)
+        return jsonify({'turn': turn})
+    except Exception as e:
+        print('Error: ', str(e))
+        print('-----------------------')
+        # return jsonify({'result': 'error', 'message': str(e)})
+        return jsonify({'error': str(e)})
 
 @app.route('/board/config_moves', methods=['POST'])
 def get_config_moves():
-    return jsonify({})
+    try:
+        data = request.get_json()
+
+        print('Value moves: ', data)
+        print('-----------------------')
+        moves = game.Board.get_config_moves(data)
+        return jsonify({'config': moves})
+    except Exception as e:
+        print('Error: ', str(e))
+        print('-----------------------')
+        # return jsonify({'result': 'error', 'message': str(e)})
+        return jsonify({'error': str(e)})
 
 @app.route('/piece/remove', methods=['POST'])
 def remove_piece():
-    return jsonify({})
+    # Access the "value" parameter from the query string
+    try:
+        data = request.get_json()
+        print('Value to remove: ', data)
+        print('-----------------------')
+        left_pieces = game.Board.remove(data)
+        return jsonify({'left_pieces': left_pieces})
+    except Exception as e:
+        print('Error: ', str(e))
+        print('-----------------------')
+        # return jsonify({'result': 'error', 'message': str(e)})
+        return jsonify({'error': str(e)})
 
 @app.route('/piece/calc_position', methods=['POST'])
 def calculate_position():
-    return jsonify({})
+    try:
+        data = request.get_json()
+
+        print('Value Piece position: ', data)
+        print('-----------------------')
+        position = game.Board.Piece.calc_pos(data)
+        return jsonify({'position': position})
+    except Exception as e:
+        print('Error: ', str(e))
+        print('-----------------------')
+        # return jsonify({'result': 'error', 'message': str(e)})
+        return jsonify({'error': str(e)})
