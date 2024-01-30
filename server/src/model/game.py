8b74548389e4d70b6bb341a35672a0d99707cda7
sobgui <i_sobgui@hetic.eu>
2024-01-30 08:55:17 +0000

    Adding Game model( init, turn) for the game serveur side

---
+++ server/src/model/game.py
@@ -0,0 +1,17 @@
+from ..config.constants import WOOD, WHITE, BLUE, SQUARE_SIZE
+from .board import Board
+
+class Game:
+    def __init__(self, ):
+        self._init()
+
+    def _init(self):
+        self.Board = Board()
+
+    def change_turn(self, turn):
+        if turn == str(WOOD):
+            turn = str(WHITE)
+        else:
+            turn = str(WOOD)
+        return turn
+
