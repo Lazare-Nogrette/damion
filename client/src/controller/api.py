fdd781bafca0891c22a20fea3d492b630ce2a070
VPoussou <v_poussou@hetic.eu>
2024-01-30 13:56:13 +0100

    Adding API consumer file controller for client side

---
+++ client/src/controller/api.py
@@ -0,0 +1,18 @@
+import requests
+from config.conf import HOST_API
+
+class GameAPI():
+    def __init__():
+        pass
+
+    def get_turn():
+        pass
+
+    def get_config():
+        pass
+
+    def remove_piece():
+        pass
+
+    def calculate_position():
+        pass
